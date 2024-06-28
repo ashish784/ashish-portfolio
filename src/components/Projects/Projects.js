10816988f1f57cb5d1596b331b60bef7f86739b9
ashish784 <mauryaashish784@gmail.com>
2024-06-29 00:15:47 +0530

    project1p

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-import webflix from "../../Assets/Projects/file.png";
+import webflix from "../../Assets/Projects/Webflix.png";
 
 function Projects() {
   return (
